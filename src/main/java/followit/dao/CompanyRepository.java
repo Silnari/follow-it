c503cd4b121df02f4acffd1e2f34632db4b5cde7
Silnari <konrad.perl@gmail.com>
2020-12-06 15:47:32 +0100

    Removed unused classes

--- src/main/java/followit/dao/CompanyRepository.java
+++
@@ -1,9 +0,0 @@
-package followit.dao;
-
-import followit.domain.Company;
-import org.springframework.data.neo4j.repository.Neo4jRepository;
-
-public interface CompanyRepository extends Neo4jRepository<Company, Long> {
-
-    Company findByCompanyName(String companyName);
-}
--- src/main/java/followit/domain/User.java
+++ src/main/java/followit/domain/User.java
@@ -7,9 +7,7 @@ import org.neo4j.ogm.annotation.Id;
 import org.neo4j.ogm.annotation.NodeEntity;
 import org.neo4j.ogm.annotation.Relationship;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @Getter
@@ -22,18 +20,14 @@ public class User {
     private String login;
     private String firstName;
     private String lastName;
-    @Relationship(type = "PARTNERSHIP")
-    private List<Partnership> sponsors;
     @Relationship(type = "FOLLOWS")
     private Set<User> following;
 
     public User() {
-        sponsors = new ArrayList<>();
         following = new HashSet<>();
     }
 
     public User(String login, String firstName, String lastName) {
-        sponsors = new ArrayList<>();
         following = new HashSet<>();
         this.login = login;
         this.firstName = firstName;
